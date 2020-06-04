9544cf476ddd274208848e6c6aa23ea6e1bc9f01
Sabari-rs1 <sabari@codingmart.com>
2020-06-04 17:04:38 +0530

    Updates

---
+++ precache-manifest.34ad6144639ab3f19c1d992d30985ccf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f83e370497c7d2eebcf013561441c3ec",
+    "url": "/infinitescrollusingIO/index.html"
+  },
+  {
+    "revision": "8a5a8dac7e0f1043d771",
+    "url": "/infinitescrollusingIO/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4df33e2c572b05532e88",
+    "url": "/infinitescrollusingIO/static/js/2.eb7cdaf8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infinitescrollusingIO/static/js/2.eb7cdaf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a5a8dac7e0f1043d771",
+    "url": "/infinitescrollusingIO/static/js/main.c90e47fb.chunk.js"
+  },
+  {
+    "revision": "1f683c22714bd27829f2",
+    "url": "/infinitescrollusingIO/static/js/runtime-main.7b97b792.js"
+  }
+]);
